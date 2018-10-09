8f55b192ad34674f3a4bf2dec0e408c1b6aa0525
Samuel Hunter <samuelhunter1024@gmail.com>
2018-10-08 21:54:11 -0700

    Initial Commit
    Signed-off-by: Samuel Hunter <samuelhunter1024@gmail.com>

---
+++ src/main/java/org/ghotid/css142/samlisp/App.java
@@ -0,0 +1,10 @@
+package org.ghotid.css142.samlisp;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
