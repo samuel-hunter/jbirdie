0e2e6c6497d599282f1e6e8deae0bd0b93a68012
Samuel Hunter <samuel@shunter.xyz>
2018-10-11 14:07:59 -0700

    Add first core methods of Lisp.
    Signed-off-by: Samuel Hunter <samuelhunter1024@gmail.com>

---
+++ src/main/java/com/ghotid/css142/jbirdie/libcore/FuncCdr.java
@@ -0,0 +1,21 @@
+package com.ghotid.css142.jbirdie.libcore;
+
+import com.ghotid.css142.jbirdie.LispEnvironment;
+import com.ghotid.css142.jbirdie.exception.ArgumentNumberException;
+import com.ghotid.css142.jbirdie.objects.ConsList;
+import com.ghotid.css142.jbirdie.objects.FuncObject;
+import com.ghotid.css142.jbirdie.objects.LispObject;
+
+/**
+ * Seventh and final function required to implement a minimal lisp.
+ */
+public class FuncCdr implements FuncObject {
+    @Override
+    public LispObject call(LispEnvironment environment, LispObject args) {
+        int size = new ConsList(args).size();
+        if (size != 1)
+            throw new ArgumentNumberException(size, "1");
+
+        return args.getCar().evaluate(environment).getCdr();
+    }
+}
---
+++ src/main/java/com/ghotid/css142/jbirdie/libcore/FuncEq.java
@@ -0,0 +1,30 @@
+package com.ghotid.css142.jbirdie.libcore;
+
+import com.ghotid.css142.jbirdie.LispEnvironment;
+import com.ghotid.css142.jbirdie.exception.ArgumentNumberException;
+import com.ghotid.css142.jbirdie.objects.*;
+
+/**
+ * Third function required to implement a minimal lisp.
+ * <p>
+ * "eq" accepts two values. It returns the symbol "t" if they both point to the
+ * same object; else, it returns nil.
+ */
+public class FuncEq implements FuncObject {
+    @Override
+    public LispObject call(LispEnvironment environment, LispObject args) {
+        ConsList argList = new ConsList(args);
+        int size = argList.size();
+
+        if (size != 2)
+            throw new ArgumentNumberException(size, "2");
+
+        if (argList.get(0).evaluate(environment) ==
+                argList.get(1).evaluate(environment)) {
+
+            return environment.getVariable("t");
+        } else {
+            return NilObject.getNil();
+        }
+    }
+}
