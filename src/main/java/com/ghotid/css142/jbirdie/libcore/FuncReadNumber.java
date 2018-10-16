ba1dfc7a36d1b0127600dbde2ee45829b0c1d013
Samuel Hunter <samuelhunter1024@gmail.com>
2018-10-15 20:59:42 -0700

    Add methods `read-line` and `read-number`
    Signed-off-by: Samuel Hunter <samuelhunter1024@gmail.com>

---
+++ src/main/java/com/ghotid/css142/jbirdie/libcore/FuncReadNumber.java
@@ -0,0 +1,22 @@
+package com.ghotid.css142.jbirdie.libcore;
+
+import com.ghotid.css142.jbirdie.environment.Environment;
+import com.ghotid.css142.jbirdie.objects.ConsList;
+import com.ghotid.css142.jbirdie.objects.FuncObject;
+import com.ghotid.css142.jbirdie.objects.LispObject;
+import com.ghotid.css142.jbirdie.objects.NumberObject;
+
+import java.util.Scanner;
+
+public class FuncReadNumber implements FuncObject {
+    @Override
+    public LispObject call(Environment environment, LispObject args) {
+        new ConsList(args).assertSizeEquals(0);
+
+        Scanner scanner = new Scanner(System.in);
+        double value = scanner.nextDouble();
+
+        // Do not close the scanner here; it will cause an IOException.
+        return new NumberObject(value);
+    }
+}
