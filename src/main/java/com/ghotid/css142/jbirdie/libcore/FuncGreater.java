1301842802f83015e45dd5af42a0fcc436726d1a
Samuel Hunter <samuelhunter1024@gmail.com>
2018-10-15 20:50:48 -0700

    Add comparison operators `<`, `>`, `=`.
    Signed-off-by: Samuel Hunter <samuelhunter1024@gmail.com>

---
+++ src/main/java/com/ghotid/css142/jbirdie/libcore/FuncGreater.java
@@ -0,0 +1,34 @@
+package com.ghotid.css142.jbirdie.libcore;
+
+import com.ghotid.css142.jbirdie.environment.Environment;
+import com.ghotid.css142.jbirdie.objects.*;
+
+/**
+ * Return true "t" only when each number given is greater than the next.
+ */
+public class FuncGreater implements FuncObject {
+    @Override
+    public LispObject call(Environment environment, LispObject args) {
+        new ConsList(args).assertSizeAtLeast(1);
+
+        double largerNum = LispObject.cast(
+                NumberObject.class,
+                args.getCar().evaluate(environment)
+        ).getValue();
+
+        for (LispObject obj : new ConsList(args.getCdr())) {
+            double smallerNum = LispObject.cast(
+                    NumberObject.class,
+                    obj.evaluate(environment)
+            ).getValue();
+
+            // Return false immediately if one of the expressions is not valid.
+            if (!(largerNum > smallerNum))
+                return NilObject.getNil();
+
+            largerNum = smallerNum;
+        }
+
+        return environment.get("t");
+    }
+}
