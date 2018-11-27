d4979f04b3c25ee82c901fa6ca3b8d9e69a655e3
Samuel Hunter <samuel@shunter.xyz>
2018-11-26 19:10:57 -0800

    Add builtins `to-codepoints`, `from-codepoints`, `char-at`.
    Signed-off-by: Samuel Hunter <samuelhunter1024@gmail.com>

--- src/main/java/com/ghotid/css142/jbirdie/builtin/LibString.java
+++ src/main/java/com/ghotid/css142/jbirdie/builtin/LibString.java
@@ -1,7 +1,11 @@
 package com.ghotid.css142.jbirdie.builtin;
 
 import com.ghotid.css142.jbirdie.InterpreterContext;
+import com.ghotid.css142.jbirdie.exception.LispException;
 import com.ghotid.css142.jbirdie.objects.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Collection of lisp functions concerning string manipulation.
@@ -29,5 +33,67 @@ public final class LibString {
         return new StringObject(
                 args.getSource(),
                 args.getCar().toString());
+    }
+
+    @BuiltinFunc(name="to-codepoints", evalArgs=true, evalResult=false,
+            doc="Convert string into a list of codepoints.")
+    public static LispObject toCodepoints(InterpreterContext context,
+                                          LispObject args) {
+        new ConsList(args).assertSizeEquals(1);
+        List<LispObject> result = new ArrayList<>();
+        String str = args.getCar().castTo(StringObject.class).toString();
+        for (int i = 0; i < str.length(); i++) {
+            result.add(new IntegerObject(args.getSource(), str.codePointAt(i)));
+        }
+
+        return ConsObject.fromList(args.getSource(), result);
+    }
+
+    @BuiltinFunc(name="from-codepoints", evalArgs=true, evalResult=false,
+            doc="Convert a list of integer-codepoints to a string.")
+    public static LispObject fromCodepoints(InterpreterContext context,
+                                                LispObject args) {
+        new ConsList(args).assertSizeEquals(1);
+        ConsList codepoints = new ConsList(args.getCar());
+        StringBuilder sb = new StringBuilder();
+        for (LispObject codepointObj : codepoints) {
+            int codepoint =
+                    codepointObj.castTo(NumberObject.class).getValue().intValue();
+
+            if (Character.isBmpCodePoint(codepoint)) {
+                // Single-char codepoint.
+                sb.append((char) codepoint);
+            } else if (Character.isValidCodePoint(codepoint)) {
+                // Two-char codepoint.
+                sb.append(Character.highSurrogate(codepoint));
+                sb.append(Character.lowSurrogate(codepoint));
+            } else {
+                throw new LispException("Invalid codepoint '" + codepoint + "'.");
+            }
+        }
+
+        return new StringObject(args.getSource(), sb.toString());
+    }
+
+    @BuiltinFunc(name="char-at", evalArgs=true, evalResult=false,
+            doc="Return the codepoint of a char at an index.")
+    public static LispObject charAt(InterpreterContext context,
+                                    LispObject args) {
+        ConsList argList = new ConsList(args);
+        argList.assertSizeEquals(2);
+
+        int index =
+                argList.get(0).castTo(NumberObject.class).getValue().intValue();
+        String str = argList.get(1).castTo(StringObject.class).getValue();
+
+        int codepoint;
+
+        try {
+            codepoint = str.codePointAt(index);
+        } catch (IndexOutOfBoundsException e) {
+            throw new LispException("Index out of bounds: " + e.getMessage());
+        }
+
+        return new IntegerObject(args.getSource(), codepoint);
     }
 }
